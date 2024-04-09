83f289c160fc27d6e8d53c4b66ee6ea87e66e0f2
TanzHasan <tanzirhasanhasan@gmail.com>
2024-04-08 23:44:17 -0400

    Initial server command

---
+++ payload.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.environ["MODAL_URL"]
+
+story_string = '''Darkrai is sent out!
+
+The opposing Landorus is sent out!
+
+The opposing Landorus's Intimidate cuts Darkrai's Attack!
+
+The opposing player withdrew Landorus!
+
+The opposing Iron Moth is sent out!
+
+The opposing Iron Moth's Booster Energy allows it to get the Quark Drive!
+
+Darkrai used Ice Beam!
+
+It's not very effective...
+
+The opposing Iron Moth was frozen solid!'''
+payload = {
+    "game_string": story_string,
+    "query": "You are commentating a pokemon battle between two trainers. Describe the battle succinctly but in an engaging way. Say no more than 50 words.",
+    "data": {
+        "pokemon": ["Darkrai", "Iron Moth", "Landorus"],
+        "abilities": ["Quark Drive", "Intimidate"],
+        "items": ["Booster Energy"],
+        "moves": ["Dark Pulse"]
+    }
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+print(response.json())
---
+++ server/__init__.py
@@ -0,0 +1,73 @@
+from modal import Image, Stub, wsgi_app
+import modal
+
+stub = Stub(
+    "pllm",
+    image=Image.debian_slim().pip_install("flask", "pymongo", "openai", "flask-cors"),
+    secrets=[modal.Secret.from_name("flask_Secrets")],
+)
+
+@stub.function()
+def unpack(data):   
+    import pymongo
+    import os
+    client = pymongo.MongoClient(os.environ["MONGO_URL"])
+    db = client["Data"]  # Replace with your actual database name
+    
+    entries = ["pokemon", "abilities", "moves", "items"]
+    ans = []
+    
+    for entry in entries:
+        collection = db[entry]  # Access the appropriate collection
+        for item in data[entry]:
+            result = collection.find_one({"name": item})
+            rstring = f"{item}: "
+            if 'descriptions' in result:
+                rstring = rstring + str(result["descriptions"])
+            if 'description' in result:
+                rstring = rstring + result["description"]
+            if 'biology' in result:
+                rstring = rstring + result["biology"]
+            ans.append(rstring)
+    return ans
+
+@stub.function()
+def trash_talk(game_string, query, data):
+    import pymongo
+    import os
+    import openai
+    entries = unpack.local(data)
+    client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    prompt = f"{query}:\n\n"
+    prompt += f"Context: {entries}"
+    response = client.chat.completions.create(
+        messages=[{
+                "role": "system",
+                "content": prompt,
+            },
+            {"role": "user", "content": game_string}],
+        model="gpt-3.5-turbo",
+    )
+    # print(response.choices[0].message.content)
+    expl = response.choices[0].message.content
+
+    return {"result": expl}
+    
+
+@stub.function()
+@wsgi_app()
+def flask_app():
+    from flask import Flask, request
+    from flask_cors import CORS, cross_origin
+
+    web_app = Flask(__name__)
+    cors = CORS(web_app)
+
+    @web_app.post("/get_battle")
+    @cross_origin()
+    def get_battle():
+        log = request.json["game_string"]
+        query = request.json["query"]
+        data = request.json["data"]
+        return trash_talk.remote(log, query, data)
+    return web_app
